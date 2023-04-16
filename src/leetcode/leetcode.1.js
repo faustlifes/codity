a27dfd7521d3415282942733d7bad3c1491792a4
tkachuk <serhii.tkachuk@visiquate.com>
2023-04-16 08:59:33 -0500

    added leetcode task #27

--- src/leetcode/leetcode.1.js
+++ src/leetcode/leetcode.1.js
@@ -467,8 +467,29 @@ class Leetcode1 extends SingletonTemplate {
     return addIndex;
   }
 
-  solution13(nums, val) {
-
+  /**
+   * 27. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+   * The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+   * Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+   *   Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+   *   The remaining elements of nums are not important as well as the size of nums.
+   * Return k.
+   *
+   * @param {number[]} nums
+   * @param {number} val
+   * @return {number}
+   *
+  * */
+  solution13(nums = [3,2,2,3], val = 3) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === val) {
+        count += 1;
+        nums[i] = -1;
+      }
+    }
+    nums.sort((a,b) => b - a);
+    return nums.length - count;
   }
 }
 
