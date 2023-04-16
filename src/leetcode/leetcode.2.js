fb6e40304626500a3b04502d11e5584e6dcff1b9
tkachuk <serhii.tkachuk@visiquate.com>
2023-04-16 12:14:34 -0500

    #69

--- src/leetcode/leetcode.2.js
+++ src/leetcode/leetcode.2.js
@@ -6,6 +6,7 @@ class Leetcode2 extends SingletonTemplate {
   static getInstance() {
     return super.getInstance(instance, Leetcode2);
   }
+
   /**
    * 28. Find the Index of the First Occurrence in a String
    * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
@@ -33,6 +34,38 @@ class Leetcode2 extends SingletonTemplate {
    * */
   solution1(haystack = 'leetcode', needle = 'leet') {
     return haystack.indexOf(needle);
+  }
+
+  /**
+   * 69. Sqrt(x)
+   * Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+   * You must not use any built-in exponent function or operator.
+   * For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+   * Example 1:
+   *   Input: x = 4
+   *   Output: 2
+   *   Explanation: The square root of 4 is 2, so we return 2.
+   * Example 2:
+   *   Input: x = 8
+   *   Output: 2
+   *   Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+   *
+   * Constraints:
+   *   0 <= x <= 231 - 1
+   * */
+  solution2(x = 2147395600) {
+
+    if (x === 0 || x === 1) {
+      return x;
+    }
+    let counter = 2;
+    while (true) {
+      if (counter * counter > x) {
+        break;
+      }
+      counter++
+    }
+    return counter - 1;
   }
 }
 
