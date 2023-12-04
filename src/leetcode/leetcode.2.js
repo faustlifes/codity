8f6fba25f5e933ed44fdaebf69ba19124cb4df72
tkachuk <serhii.tkachuk@visiquate.com>
2023-12-04 09:47:37 -0600

    added leetcode 66 task

--- src/leetcode/leetcode.2.js
+++ src/leetcode/leetcode.2.js
@@ -244,6 +244,31 @@ class Leetcode2 extends SingletonTemplate {
     return sL[sL.length -1].length;
   }
 
+  /** 66. Plus One
+   * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+   * Increment the large integer by one and return the resulting array of digits.
+   * Example 1:
+   *  Input: digits = [1,2,3]
+   *  Output: [1,2,4]
+   *  Explanation: The array represents the integer 123.
+   *  Incrementing by one gives 123 + 1 = 124.
+   *  Thus, the result should be [1,2,4].
+   * @param {number[]} digits
+   * @return {number[]}
+  * */
+  solution66(digits = [1,9]) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      } else {
+        digits[i] = 0;
+      }
+    }
+    digits.unshift(1);
+    return digits;
+  }
+
 }
 
 export default Leetcode2.getInstance();
