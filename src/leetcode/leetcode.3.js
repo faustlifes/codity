afa4675756c0e9b05fa8e4c3c881c5487ab411b3
tkachuk <serhii.tkachuk@visiquate.com>
2024-06-09 17:56:52 -0500

    added tasks

--- src/leetcode/leetcode.3.js
+++ src/leetcode/leetcode.3.js
@@ -376,6 +376,38 @@ class LeetCode3 {
     }
     return queries;
   }
+  /** 824. Goat Latin
+   * You are given a string sentence that consist of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+   * We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.) The rules of Goat Latin are as follows:
+   * If a word begins with a vowel ('a', 'e', 'i', 'o', or 'u'), append "ma" to the end of the word.
+   * For example, the word "apple" becomes "applema".
+   * If a word begins with a consonant (i.e., not a vowel), remove the first letter and append it to the end, then add "ma".
+   * For example, the word "goat" becomes "oatgma".
+   * Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+   * For example, the first word gets "a" added to the end, the second word gets "aa" added to the end, and so on.
+   * Return the final sentence representing the conversion from sentence to Goat Latin.
+   * Example 1:
+   *   Input: sentence = "I speak Goat Latin"
+   *   Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+   * Example 2:
+   *   Input: sentence = "The quick brown fox jumped over the lazy dog"
+   *   Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+   * @param {string} S
+   * @return {string}
+   */
+  solution(S) {
+    const wordList = S.split(' ');
+    const vowels = ['A','a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
+    return wordList.map((word, i) => {
+      let newWord = word;
+      if (!vowels.includes(word[0])) {
+        newWord = word.substring(1,word.length) + word[0];
+      }
+      newWord += 'ma'+'a'.repeat(i+1);
+      return newWord;
+    }).join(' ');
+  }
+
 }
 
 export default LeetCode3;
