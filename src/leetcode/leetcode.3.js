9d798f72016c10521995271596a021dc255273a5
tkachuk <serhii.tkachuk@visiquate.com>
2024-06-09 17:59:00 -0500

    added tasks

--- src/leetcode/leetcode.3.js
+++ src/leetcode/leetcode.3.js
@@ -407,6 +407,40 @@ class LeetCode3 {
       return newWord;
     }).join(' ');
   }
+  /** 807. Max Increase to Keep City Skyline
+   * There is a city composed of n x n blocks, where each block contains a single building shaped like a vertical square prism. You are given a 0-indexed n x n integer matrix grid where grid[r][c] represents the height of the building located in the block at row r and column c.
+   * A city's skyline is the outer contour formed by all the building when viewing the side of the city from a distance. The skyline from each cardinal direction north, east, south, and west may be different.
+   * We are allowed to increase the height of any number of buildings by any amount (the amount can be different per building). The height of a 0-height building can also be increased. However, increasing the height of a building should not affect the city's skyline from any cardinal direction.
+   * Return the maximum total sum that the height of the buildings can be increased by without changing the city's skyline from any cardinal direction.
+   * @param {number[][]} grid
+   * @return {number}
+   */
+  solution807(grid) {
+    const l = grid.length;
+    let diff = 0;
+    const rawMaxes = [];
+    const colMaxes = [];
+
+    for (let row = 0; row < l; row++) {
+      rawMaxes[row] = 0;
+      for (let col = 0; col < grid[row].length; col++) {
+        if (row === 0) {
+          colMaxes[col] = 0;
+        }
+        rawMaxes[row] = Math.max(rawMaxes[row], grid[row][col]);
+        colMaxes[col] = Math.max(colMaxes[col], grid[row][col]);
+      }
+    }
+
+    for (let row = 0; row < l; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        const max = Math.min(rawMaxes[row], colMaxes[col]);
+        diff += max - grid[row][col]
+      }
+    }
+    return diff;
+  }
+
 
 }
 
