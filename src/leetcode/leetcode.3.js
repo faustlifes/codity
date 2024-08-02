d607ca0a261df5e07b501a8436157ddf2b1ee01a
tkachuk <serhii.tkachuk@visiquate.com>
2024-08-02 17:27:25 -0500

    added lt task 3

--- src/leetcode/leetcode.3.js
+++ src/leetcode/leetcode.3.js
@@ -776,6 +776,40 @@ class LeetCode3 {
       else return revX;
     } else return 0;
   }
+  /** 3. Longest Substring Without Repeating Characters
+   * Given a string s, find the length of the longest substring without repeating characters.
+   * Example 1:
+   *   Input: s = "abcabcbb"
+   *   Output: 3
+   *   Explanation: The answer is "abc", with the length of 3.
+   * Example 2:
+   *   Input: s = "bbbbb"
+   *   Output: 1
+   *   Explanation: The answer is "b", with the length of 1.
+   * Example 3:
+   *   Input: s = "pwwkew"
+   *   Output: 3
+   *   Explanation: The answer is "wke", with the length of 3.
+   *   Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+   * @param {string} s
+   * @return {number}
+   * */
+  solution3(s) {
+    let maxLength = 0;
+    let left = 0;
+    let lastSeen = {};
+
+    for (let right = 0; right < s.length; right++) {
+      let c = s.charAt(right);
+      if (c in lastSeen && lastSeen[c] >= left) {
+        left = lastSeen[c] + 1;
+      }
+      maxLength = Math.max(maxLength, right - left + 1);
+      lastSeen[c] = right;
+    }
+
+    return maxLength;
+  }
 }
 
 export default LeetCode3;
